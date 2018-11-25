33324ec8409440574200c7805b46b81ffc8ebc7e
Breedon.M <breedon.matthew@gmail.com>
2018-11-25 15:11:55 +0100

    divider func

--- math.js
+++ math.js
@@ -8,6 +8,15 @@ const subtractNumbers = function(a,b) {
 };
 
 
+const divFunc = function(a,b){
+  return a / b;
+}
+//  in the file math.js add a function to divide two numbers at 
+//the end of the file. the function should take two numbers as arguments
+// (e.g., a and b) and return the result of their division (e.g. a/b)
+
+
+console.log(divFunc(5,5));
 console.log(addNumbers());
 
 console.log(subtractNumbers());
